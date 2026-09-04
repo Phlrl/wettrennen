5a21d7255d1a131d85ea633f61f50746bab44ee1
Phlrl <philipp129999@gmail.com>
2026-09-04 16:31:48 +0200

    class fahrad implemented

---
+++ Wettrennnen/Fahrzeuge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wettrennnen
+{
+    abstract class Fahrzeug
+    {
+        protected int geschwindigkeit = 0;
+    }
+
+    class Fahrrad: Fahrzeug
+    {
+
+    }
+}
