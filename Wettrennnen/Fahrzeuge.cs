e02af53b7db08c9e886800d6417f272cecdb8d69
Phlrl <philipp129999@gmail.com>
2026-09-04 18:21:53 +0200

    wheels, new methods

--- Wettrennnen/Fahrzeuge.cs
+++ Wettrennnen/Fahrzeuge.cs
@@ -6,10 +6,48 @@ namespace Wettrennnen
 {
     abstract class Fahrzeug
     {
-        protected int geschwindigkeit = 0;
+        protected double Geschwindigkeit = 0; //kmh
+        protected double Position = 0; //km
+
+
+        protected void bewege (double Minutes)
+        {
+            double t = Minutes / 60;
+            Position += Geschwindigkeit * t;
+        }
+
+        protected void SetzeGeschwindigkeit()
+        {
+
+        }
+        protected int Raeder;
     }
 
     class Fahrrad: Fahrzeug
+    {
+        public Fahrrad()
+        {
+            Raeder = 2;
+
+            Fahrrad fahrrad = new Fahrrad();
+        }
+    }
+
+    class Auto: Fahrzeug
+    {
+        public Auto()
+        {
+            Raeder = 4;
+            Auto auto = new Auto();
+        }
+    }
+
+    class Rennwagen : Auto
+    {
+        
+    }
+
+    class Krankenwagen : Auto
     {
 
     }
