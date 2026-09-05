ff75d0b836fdd4a836313e5e940bf3f52abbffeb
Phlrl <philipp129999@gmail.com>
2026-09-05 16:07:00 +0200

    new test methods

--- Wettrennnen/Program.cs
+++ Wettrennnen/Program.cs
@@ -6,10 +6,16 @@ namespace Wettrennnen
     {
         static void Main(string[] args)
         {
+            test();
+        }
+
+        static void test()
+        {
+            //in test werden fahrrad usw generiert und es wird mit der methode setze geschwindgkeit die geschwindigkeit gesetzt
             Fahrrad fahrrad = new Fahrrad();
             fahrrad.SetzeGeschwindigkeit(20);
 
-            Auto auto= new Auto();
+            Auto auto = new Auto();
             auto.SetzeGeschwindigkeit(150);
 
             Rennwagen rennwagen = new Rennwagen();
@@ -17,6 +23,8 @@ namespace Wettrennnen
 
             Krankenwagen krankenwagen = new Krankenwagen();
             krankenwagen.SetzeGeschwindigkeit(80);
+
+            //dann wird jedes objekt mit der methode bewege bewegt und in () steht der wert in min
 
             fahrrad.bewege(240);
 
@@ -27,6 +35,40 @@ namespace Wettrennnen
             rennwagen.bewege(60);
 
             krankenwagen.bewege(60);
+            krankenwagen.BlaulichtAn();
+        }
+
+        static void test2()
+        {
+
+            //list für alle farzeuge über den loop werden sie aus der liste gepullt und in die for each schleife gepacktg wo sie dann mit der methode bewege 1 h lang fahren.
+            List<Fahrzeug> fahrzeuge = new List<Fahrzeug>();
+
+            Fahrrad fahrrad = new Fahrrad();
+            fahrrad.SetzeGeschwindigkeit(20);
+            fahrzeuge.Add(fahrrad); //Addet zur list fahrzeuge
+            fahrrad.bewege(240);
+
+            Auto auto = new Auto();
+            auto.SetzeGeschwindigkeit(150);
+            fahrzeuge.Add(auto);
+
+            Rennwagen rennwagen = new Rennwagen();
+            rennwagen.SetzeGeschwindigkeit(200);
+            fahrzeuge.Add(rennwagen);
+
+            Krankenwagen krankenwagen = new Krankenwagen();
+            krankenwagen.SetzeGeschwindigkeit(80);
+            fahrzeuge.Add(krankenwagen);
+
+            foreach (var fahrzeug in fahrzeuge)
+            {
+                fahrzeug.bewege(60);
+
+                // fahrzeug.blaulichtAn(); geht nicht fahrzeug.blaulichtAn() nicht eine methode von der klasse fahrzeug ist sondern nur der klasse krankenwagen
+            }
+
+
         }
     }
 }
